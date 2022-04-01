f9474b3063b2914de450ab448d5977a7bf459ecb
Tarde <Tarde@PC18-T3INF>
2022-04-01 16:26:45 +0200

    Prueba de la subida de un proyecto a github

---
+++ Desktop/OtrosProyectosJava/ProyectoPractica4.4.2/src/proyectopractica4/pkg4/pkg2/ProyectoPractica442.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyectopractica4.pkg4.pkg2;
+
+import java.time.Clock;
+
+/**
+ *
+ * @author Tarde
+ */
+public class ProyectoPractica442 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Introduzca su nombre: ");
+    }
+}
